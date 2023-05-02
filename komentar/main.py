6abe26cb97f93b76d89a800cd679bfdfcd55fb31
novalagung <hello@novalagung.com>
2023-05-02 16:55:51 +0700

    feat: add more examples

---
+++ komentar/main.py
@@ -0,0 +1,12 @@
+# ini adalah komentar
+print("halo,")
+print("selamat pagi!") # ini juga komentar
+
+# println("statement ini tidak akan dipanggil")
+
+"""
+ini juga komentar,
+tapi multi-bari
+"""
+
+print("tetap semangat 🚀")
