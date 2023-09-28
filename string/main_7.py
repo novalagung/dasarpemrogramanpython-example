919c2915501e088b02221056127affc96543a012
novalagung <hello@novalagung.com>
2023-09-28 17:13:16 +0700

    feat: update

---
+++ string/main_7.py
@@ -0,0 +1,92 @@
+# %% A.17.7. Operasi character & case
+
+# %%
+
+print("islower()")
+print("hello python".islower())
+print("Hello Python".islower())
+print("HELLO PYTHON".islower())
+
+print("istitle()")
+print("hello python".istitle())
+print("Hello Python".istitle())
+print("HELLO PYTHON".istitle())
+
+print("isupper()")
+print("hello python".isupper())
+print("Hello Python".isupper())
+print("HELLO PYTHON".isupper())
+
+# %% ◉ Pengecekan karakter alfabet dan angka
+
+print("isalpha()")
+print("abcdef".isalpha())
+print("abc123".isalpha())
+print("موز".isalpha())
+print("バナナ".isalpha())
+
+print("isdigit()")
+print("123456".isdigit())
+print("123abc".isdigit())
+print('⅓'.isdigit())
+print('4²'.isdigit())
+print('٢٨'.isdigit())
+print('𝟜'.isdigit())
+
+print("isdecimal()")
+print("123456".isdecimal())
+print("123abc".isdecimal())
+print('⅓'.isdecimal())
+print('4²'.isdecimal())
+print('٢٨'.isdecimal())
+print('𝟜'.isdecimal())
+
+print("isnumeric()")
+print("123456".isnumeric())
+print("123abc".isnumeric())
+print('⅓'.isnumeric())
+print('4²'.isnumeric())
+print('٢٨'.isnumeric())
+print('𝟜'.isnumeric())
+
+print("isalnum()")
+print("123abc".isalnum())
+print("12345⅓".isalnum())
+print("abcdef".isalnum())
+print("abc 12".isalnum())
+print("موز".isalnum())
+print("バナナ".isalnum())
+
+# %% ◉ Pengecekan karakter whitespace
+
+print("isspace()")
+print(" ".isspace())
+print("\n".isspace())
+print("\n\r".isspace())
+
+# %% ◉ Pengecekan karakter case
+
+print("islower()")
+print("hello python".islower())
+print("Hello Python".islower())
+print("HELLO PYTHON".islower())
+
+print("istitle()")
+print("hello python".istitle())
+print("Hello Python".istitle())
+print("HELLO PYTHON".istitle())
+
+print("isupper()")
+print("hello python".isupper())
+print("Hello Python".isupper())
+print("HELLO PYTHON".isupper())
+
+# %% ◉ Mengubah karakter case
+
+print("hello python".capitalize())
+print("hello python".title())
+print("hello python".upper())
+print("Hello Python".lower())
+print("Hello Python".swapcase())
+
+# %%
