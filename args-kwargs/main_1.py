a6530c92b5183dd90977ef88ea853b96080310c1
novalagung <hello@novalagung.com>
2023-10-09 15:35:37 +0700

    feat: update

--- args-kwargs/main_1.py
+++ args-kwargs/main_1.py
@@ -34,3 +34,13 @@ def sum_then_print(message, *numbers):
     print(f"{message} {total}")
 
 sum_then_print("total nilai:", 2, 3, 4, 5, 4)
+
+# %% ◉ Kombinasi positional argument dan args
+
+def sum_then_print(message, *numbers, suffix_message):
+    total = 0
+    for n in numbers:
+        total = total + n
+    print(f"{message} {total} {suffix_message}")
+
+sum_then_print("total nilai:", 2, 3, 4, 5, 4, suffix_message="selesai!")
