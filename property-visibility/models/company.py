7753a380e95c08f0ef64c905dd713d3394559e21
novalagung <hello@novalagung.com>
2023-10-30 19:46:17 +0700

    feat: update example

--- property-visibility/models/company.py
+++ property-visibility/models/company.py
@@ -3,9 +3,16 @@ class Company:
     def __init__(self, name = "", products =[]):
         self.name = name
         self.products = products
+        self.__version = 1.0
     
     def info(self):
+        self.__print_name()
+        self.__print_products()
+
+    def __print_name(self):
         print(f"company name: {self.name}")
+
+    def __print_products(self):
         print(f"products:")
         for p in self.products:
             print(f"  -> {p.name} ({p.category})")
--- property-visibility/models/product.py
+++ property-visibility/models/product.py
@@ -3,3 +3,4 @@ class Product:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.__version = 1.0
