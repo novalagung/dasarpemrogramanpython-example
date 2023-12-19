e343c3a7612b3c699100b0804158e7b090961c25
novalagung <hello@novalagung.com>
2023-12-19 16:29:39 +0700

    feat: add json example

---
+++ json/main_2.py
@@ -0,0 +1,33 @@
+# %% A.49.1. Implementasi JSON di Python
+
+# %% ◉ Decode JSON string ke data Python
+
+import json
+
+jstr1 = '{ "name": "Maiev Shadowsong", "affliations": ["Warden", "Alliance"], "age": 10000, "active": true }'
+data1 = json.loads(jstr1)
+
+print(f"type: {type(data1).__name__}")
+
+for key in data1:
+    print(f"{key}: {data1[key]}")
+
+# %%
+
+jstr2 = """
+[{
+    "name": "Maiev Shadowsong",
+    "affliations": ["Warden", "Alliance"],
+    "age": 10000
+}, {
+    "name": "Illidan Stormrage",
+    "affliations": ["Illidari", "Armies of Legionfall"],
+    "age": 15000
+}]
+"""
+data2 = json.loads(jstr2)
+
+print(f"type: {type(data2).__name__}")
+
+for row in data2:
+    print(f"-> name: {row["name"]}, afflications: {row["affliations"]}, age: {row["age"]}")
---
+++ json/main_4.py
@@ -0,0 +1,12 @@
+# %% A.49.1. Implementasi JSON di Python
+
+# %% ◉ Membaca JSON file
+
+import json
+
+data = []
+with open('data.json', 'r') as f:
+    data = json.loads(f.read())
+
+for key in data:
+    print(f"{key}: {data[key]}")
