b67bda1973872db4ef6342ff1e4a7ba64d2bae69
novalagung <hello@novalagung.com>
2023-12-24 17:24:54 +0700

    feat: update

--- string/main_4.py
+++ string/main_4.py
@@ -1,4 +1,6 @@
-# %% A.17.4. Penggabungan string (concatenation)
+# A.17.4. Penggabungan string & split string
+
+# %% ◉ Penggabungan string (concatenation)
 
 text = "hello " "python"
 print(text)
@@ -16,3 +18,9 @@ print(message)
 
 text = " ".join(["hello", "python"])
 print(text)
+
+# %% ◉ Split string
+
+text = "hello python 12345"
+res = text.split(" ")
+print(res)
