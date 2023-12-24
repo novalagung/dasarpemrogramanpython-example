4e7a7e204af37063146be01d31aa38fd59da9829
novalagung <hello@novalagung.com>
2023-12-24 16:32:04 +0700

    feat: update source code

---
+++ pattern-matching/main_1.py
@@ -0,0 +1,47 @@
+# A.53.1. Pengenalan pattern matching
+
+command = input("Enter command: ")
+
+if command == "greet":
+    name = input("Your name: ")
+    print("hello", name)
+
+elif command == "find sum of numbers":
+    numbers = input("The numbers separated by space: ")
+    total = 0
+    for str in numbers.split(' '):
+        total = total + int(str)
+    print("total:", total)
+
+elif command == "lucky number":
+    import random
+    n = random.randint(0, 100)
+    print("your lucky number:", n)
+
+else:
+    print("unrecognized command")
+
+# %%
+
+command = input("Enter command: ")
+
+match command:
+
+    case "greet":
+        name = input("Your name: ")
+        print("hello", name)
+
+    case "sum_numbers":
+        numbers = input("The numbers separated by space: ")
+        total = 0
+        for str in numbers.split(' '):
+            total = total + int(str)
+        print("total:", total)
+
+    case "lucky_number":
+        import random
+        n = random.randint(0, 100)
+        print("your lucky number:", n)
+    
+    case _:
+        print("unrecognized command")
---
+++ pattern-matching/main_2.py
@@ -0,0 +1,109 @@
+# %% A.53.2. Pencocokan pola data sequence
+
+command = input("Enter command: ")
+inputs = command.split(' ')
+
+match inputs:
+
+    case ["greet"]:
+        name = input("Your name: ")
+        print("hello", name)
+
+    case ["greet", "thrall"]:
+        print("hello Thrall, how is the horde?")
+
+    case ["sum_numbers"]:
+        numbers = input("The numbers separated by space: ")
+        total = 0
+        for str in numbers.split(' '):
+            total = total + int(str)
+        print("total:", total)
+
+    case _:
+        print("unrecognized command")
+
+# %% ◉ Pencocokan sebagian pola
+
+command = input("Enter command: ")
+
+match command.split(' '):
+
+    case ["greet"]:
+        name = input("Your name: ")
+        print("hello", name)
+
+    case ["greet", "thrall"]:
+        print("hello noval, how is the horde?")
+
+    case ["greet", name]:
+        print("hello", name)
+
+    case _:
+        print("unrecognized command")
+
+# %% ◉ Pencocokan pola `*args`
+
+command = input("Enter command: ")
+
+match command.split(' '):
+
+    case ["greet"]:
+        name = input("Your name: ")
+        print("hello", name)
+
+    case ["greet", "thrall"]:
+        print("hello noval, how is the horde?")
+
+    case ["greet", name]:
+        print("hello", name)
+
+    case ["sum_numbers"]:
+        numbers = input("The numbers separated by space: ")
+        total = 0
+        for str in numbers.split(' '):
+            total = total + int(str)
+        print("total:", total)
+
+    case ["sum_numbers", *args]:
+        total = 0
+        for str in args:
+            total = total + int(str)
+        print("total:", total)
+    
+    case _:
+        print("unrecognized command")
+
+# %% Pencocokan pola wildcard
+
+command = input("Enter command: ")
+
+match command.split(' '):
+
+    case ["greet"]:
+        name = input("Your name: ")
+        print("hello", name)
+
+    case ["greet", "thrall"]:
+        print("hello noval, how is the horde?")
+
+    case ["greet", name]:
+        print("hello", name)
+
+    case ["sum_numbers"]:
+        numbers = input("The numbers separated by space: ")
+        total = 0
+        for str in numbers.split(' '):
+            total = total + int(str)
+        print("total:", total)
+
+    case ["sum_numbers", *args]:
+        total = 0
+        for str in args:
+            total = total + int(str)
+        print("total:", total)
+
+    case [other]:
+        print(f"unrecognized command {other}")
+
+    case other:
+        print(f"command {other} is not recognized")
